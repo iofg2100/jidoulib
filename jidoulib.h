a463c562294fa3e5d3e3aa52bfe8fd5accb7b3d8
Ryohei Ikegami <sup21-home@khc.biglobe.ne.jp>
2012-08-22 16:52:24 +0900

    ピン配置の記述を追加

--- jidoulib.h
+++ jidoulib.h
@@ -4,6 +4,41 @@
 #include <stdint.h>
 
 /**
+	ピン配置
+	
+	ADC0~4 : ラインセンサ1~5
+	PB0, PB1 エンコーダ1
+	PB2, PB3 エンコーダ2
+	
+	モータドライバ
+	
+	OC0A : PWM1
+	PB4 : DIRA1
+	PB5 : DIRB1
+	
+	OC0B : PWM2
+	PD2 : DIRA2
+	PD3 : DIRB2
+	
+	PD4 : スイッチ
+	
+	
+	自動機2専用
+	
+	PB0 : スイッチ
+	OC1A : サーボモータ用PWM
+	PB2, PB3 : モータドライバ
+	
+	PWMは20kHzまで
+*/
+
+typedef enum
+{
+	JL_RIGHT,
+	JL_LEFT
+} JL_DIRECTION;
+
+/**
 	USART初期化
 */
 void usartInit();
@@ -26,6 +61,9 @@ void adConversionInit();
 */
 uint8_t adConversionGet(uint8_t pin);
 
-
+/**
+	ロータリーエンコーダーの値を取得
+*/
+uint8_t rotaryGet(JL_DIRECTION dir);
 
 #endif
